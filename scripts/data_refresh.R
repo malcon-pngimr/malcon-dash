423e6d38fba63389251a0b3d84be77c81d50c875
myominnoo <dr.myominnoo@gmail.com>
2022-02-11 11:50:05 +1000

    update dashboard

--- scripts/data_refresh.R
+++ scripts/data_refresh.R
@@ -8,8 +8,16 @@ fid <- c(form1 = "hfc_v3", form2 = "pi_v3",
          form3 = "ppa_v3", form4 = "ei_v3", 
          form5 = "fvs_v1")
 raw_url <- "raw.githubusercontent.com/myominnoo/malcon/main/data/"
-# lapply(paste("https://", api_key, "@", raw_url, fid, ".csv", sep = ""), 
-#        read.csv, stringsAsFactors = FALSE)
 
+for (x in 1:length(fid)) {
+    temp <- tempdir()
+    
+    temp_file <- paste(temp, "/", fid[x], ".csv", sep = "")
+    download.file(paste("https://", api_key, "@", raw_url, fid[x], ".csv", sep = ""), 
+                  temp_file)
+    
+    ds <- read.csv(temp_file, stringsAsFactors = FALSE)
+    names(ds)
+}
 
 rmarkdown::render_site()
