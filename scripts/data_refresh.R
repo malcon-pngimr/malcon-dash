84e7b325438abe5af4184d408084bbd930d65f5f
myominnoo <dr.myominnoo@gmail.com>
2022-02-11 15:18:15 +1000

    test keys

--- scripts/data_refresh.R
+++ scripts/data_refresh.R
@@ -1,23 +1,40 @@
+
+# -------------------------------------------------------------------------
+# Pulling HFS data for the MalCon Project
+# Source: ODK Central - Swiss TPH
+# -------------------------------------------------------------------------
+
+# install required packages
 install.packages("flexdashboard", dependencies = TRUE)
 
-api_key <- commandArgs(trailingOnly = TRUE)
-print(paste("this is api key", api_key, "... "))
 
-## set form id and url as characters
-fid <- c(form1 = "hfc_v3", form2 = "pi_v3", 
-         form3 = "ppa_v3", form4 = "ei_v3", 
+# Set Global Environment --------------------------------------------------
+
+# Set PAT_KEY
+(pat_key <- Sys.getenv("PAT_KEY"))
+
+# Set form id and url as characters
+fid <- c(form1 = "hfc_v3", form2 = "pi_v3",
+         form3 = "ppa_v3", form4 = "ei_v3",
          form5 = "fvs_v1")
+
+# Set ODK form url
+fid_url <- paste("https://odk-central.swisstph.ch/v1/projects/17/forms/",
+                 fid, ".svc", sep = "")
+
+# Set current raw file url from repo 
 raw_url <- "raw.githubusercontent.com/myominnoo/malcon/main/data/"
 
+
 for (x in 1:length(fid)) {
-    temp <- tempdir()
+    temp_file <- tempdir()
     
     temp_file <- paste(temp, "/", fid[x], ".csv", sep = "")
-    download.file(paste("https://", api_key, "@", raw_url, fid[x], ".csv", sep = ""), 
+    download.file(paste("https://", pat_key, "@", raw_url, fid[x], ".csv", sep = ""), 
                   temp_file)
     
     ds <- read.csv(temp_file, stringsAsFactors = FALSE)
-    names(ds)
+    print(names(ds))
 }
 
-rmarkdown::render_site()
+# rmarkdown::render_site()
