0b30c07a86669b1ba35970c9c54c8361ae9da422
myominnoo <dr.myominnoo@gmail.com>
2022-02-15 23:52:34 +1000

    add install packages

--- scripts/data_refresh.R
+++ scripts/data_refresh.R
@@ -1,27 +1,19 @@
-# Set PAT_KEY
-# (pat_key <- Sys.getenv("PAT_KEY"))
-
-# Set form id and url as characters
-fid <- c(form1 = "hfc_v3", form2 = "pi_v3",
-         form3 = "ppa_v3", form4 = "ei_v3",
-         form5 = "fvs_v1")
-
-
-# Set current raw file url from repo 
-url_data_old <- "raw.githubusercontent.com/myominnoo/malcon/main/data/"
-
-# ## download all form submissions
-# for (x in 1:length(fid)) {
-#     # Compare data with current data
-#     assign(fid[x], read.csv(
-#         paste0("https://", pat_key, "@", url_data_old, fid[x], ".csv"), 
-#         stringsAsFactors = FALSE))
-#     cat("successfully imported ", fid[x], " ... \n")
-# }
-
-
-print(ls())
 
 # packages ----------------------------------------------------------------
 
+# installing package imports packages
+pkg_list <- c("mapview", "sf", "leafpop", "rgeos", "rnaturalearth", "plotly")
+
+install.packages(pkgs = pkg_list, repos = "https://cran.rstudio.com/")
+
+install.packages("rnaturalearthhires", repos = "http://packages.ropensci.org", type = "source")
+
+for(i in c(pkg_list,"rnaturalearthhires")){
+    
+    if(!i %in% rownames(installed.packages())){
+        stop(paste("Package", i, "is not available"))
+    }
+}
+
 install.packages("flexdashboard")
+
