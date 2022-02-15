3db052277fb07b55b032444c4c626f81ef2ed937
myominnoo <dr.myominnoo@gmail.com>
2022-02-15 23:48:08 +1000

    test 3

--- scripts/data_refresh.R
+++ scripts/data_refresh.R
@@ -1,5 +1,5 @@
 # Set PAT_KEY
-(pat_key <- Sys.getenv("PAT_KEY"))
+# (pat_key <- Sys.getenv("PAT_KEY"))
 
 # Set form id and url as characters
 fid <- c(form1 = "hfc_v3", form2 = "pi_v3",
@@ -19,11 +19,6 @@ url_data_old <- "raw.githubusercontent.com/myominnoo/malcon/main/data/"
 #     cat("successfully imported ", fid[x], " ... \n")
 # }
 
-hfc_v3 <- readr::read_csv(paste0("https://", pat_key, "@", url_data_old, fid[1], ".csv"))
-pi_v3 <- readr::read_csv(paste0("https://", pat_key, "@", url_data_old, fid[2], ".csv"))
-ppa_v3 <- readr::read_csv(paste0("https://", pat_key, "@", url_data_old, fid[3], ".csv"))
-ei_v3 <- readr::read_csv(paste0("https://", pat_key, "@", url_data_old, fid[4], ".csv"))
-fvs_v3 <- readr::read_csv(paste0("https://", pat_key, "@", url_data_old, fid[5], ".csv"))
 
 print(ls())
 
