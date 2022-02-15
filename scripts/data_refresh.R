eac08d7e0a62a72c79f3a68ac91d15aa0ddd175a
myominnoo <dr.myominnoo@gmail.com>
2022-02-15 23:43:29 +1000

    test 2

--- scripts/data_refresh.R
+++ scripts/data_refresh.R
@@ -10,14 +10,20 @@ fid <- c(form1 = "hfc_v3", form2 = "pi_v3",
 # Set current raw file url from repo 
 url_data_old <- "raw.githubusercontent.com/myominnoo/malcon/main/data/"
 
-## download all form submissions
-for (x in 1:length(fid)) {
-    # Compare data with current data
-    assign(fid[x], read.csv(
-        paste0("https://", pat_key, "@", url_data_old, fid[x], ".csv"), 
-        stringsAsFactors = FALSE))
-    cat("successfully imported ", fid[x], " ... \n")
-}
+# ## download all form submissions
+# for (x in 1:length(fid)) {
+#     # Compare data with current data
+#     assign(fid[x], read.csv(
+#         paste0("https://", pat_key, "@", url_data_old, fid[x], ".csv"), 
+#         stringsAsFactors = FALSE))
+#     cat("successfully imported ", fid[x], " ... \n")
+# }
+
+hfc_v3 <- readr::read_csv(paste0("https://", pat_key, "@", url_data_old, fid[1], ".csv"))
+pi_v3 <- readr::read_csv(paste0("https://", pat_key, "@", url_data_old, fid[2], ".csv"))
+ppa_v3 <- readr::read_csv(paste0("https://", pat_key, "@", url_data_old, fid[3], ".csv"))
+ei_v3 <- readr::read_csv(paste0("https://", pat_key, "@", url_data_old, fid[4], ".csv"))
+fvs_v3 <- readr::read_csv(paste0("https://", pat_key, "@", url_data_old, fid[5], ".csv"))
 
 print(ls())
 
