1a0527ac08c3a541def7673ca315259d016f5085
myominnoo <dr.myominnoo@gmail.com>
2022-02-17 16:22:01 +1000

    test GHA

--- scripts/data_refresh.sh
+++ scripts/data_refresh.sh
@@ -7,12 +7,12 @@ ls docs
 # Rscript -e "rmarkdown::render_site()"
 
 
-if [[ "$(git status --porcelain)" != "" ]]; then
-    git config --global user.name 'myominnoo'
-    git config --global user.email 'dr.myominnoo@gmail.com'
-    git add -A
-    git commit -m "Auto pull of the HFS data on $(date)"
-    git push origin 
-else
-    echo "Nothing to commit..."
-fi
+# if [[ "$(git status --porcelain)" != "" ]]; then
+#     git config --global user.name 'myominnoo'
+#     git config --global user.email 'dr.myominnoo@gmail.com'
+#     git add -A
+#     git commit -m "Auto pull of the HFS data on $(date)"
+#     git push origin 
+# else
+#     echo "Nothing to commit..."
+# fi
